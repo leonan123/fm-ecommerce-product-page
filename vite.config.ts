7b7ee37ee3fc1e19523424a7711614207f722549
leonan123 <leonanorth3@gmail.com>
2025-01-08 12:16:36 -0300

    chore: build options

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/fm-ecommerce-product-page/',
+  build: { 
+    rollupOptions: {
+      output: {
+        assetFileNames: 'src/assets/[name].[hash][extname]'
+      },
+    },
+  },
 })
