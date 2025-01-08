bc5585a38dddc37564e9073c44b13271ecfdd8b6
leonan123 <leonanorth3@gmail.com>
2025-01-08 12:33:18 -0300

    chore: vite static copy

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/images/*',
+          dest: 'assets/images',
+        },
+      ],
+    }),
+  ],
   base: '/fm-ecommerce-product-page/',
   publicDir: 'src/assets/images',
-  build: { 
+  build: {
     rollupOptions: {
       output: {
-        assetFileNames: 'src/assets/images/[name].[hash][extname]'
+        assetFileNames: 'src/assets/images/[name].[hash][extname]',
       },
     },
   },
